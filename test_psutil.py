77d62046fd83acbb00a475a7757b1f3f824bd9ad
Esteban <esteban.salazar@utp.edu.co>
2023-04-09 15:46:41 -0500

    Desarrollo de funcion auxiliar para analisis de arrays con informacion en bytes, ademas de finalizado el analisis de funciones que actuan sobre la memoria RAM

--- test_psutil.py
+++ test_psutil.py
@@ -1,17 +1,39 @@
 import psutil
-import time
 
-# Utilidades auxiliares
-start_time = time.time()
+## Codigo Auxiliar para las pruebas
+# Funcion que convierte arrays con bytes a gigabytes
+def bToB (memory, save = None, convert_indexes = []):
+    
+    memoryGB = []
+    
+    for i, val in enumerate(memory):
+        if i in convert_indexes:
+            memoryGB.append(val / (1000 ** 3))
+        else:
+            memoryGB.append(val)
+    
+    memoryGB = [round(i, 2) for i in memoryGB]
+    
+    if save is not None:
+        return memoryGB
+    else:
+        print(memoryGB)
 
 ## Contador de nucleos e hilos del procesador
 #logical_cpu_count = psutil.cpu_count()
 #fisical_cpu_count = psutil.cpu_count(False)
 
-#"" Averigua la maxima frecuencia del procesador
+## Averigua la maxima frecuencia del procesador
 #freq = psutil.cpu_freq()
 
-## Calcular carga del procesador en los ultimos minutos, en windows debe ser ejecutado de manera ciclica, basado en la cantidad de nucleos
-load = [i / psutil.cpu_count() * 100 for i in psutil.getloadavg()]
+## Determina distintos valores sobre la memoria RAM del sistema: Total, disponible, y cantidad siendo usada
+#mem = psutil.virtual_memory()
+#bToB(mem, convert_indexes=[0, 1, 3, 4])
+#print(mem)
 
-print(load)
+## Memoria SWAP
+swapsito = psutil.swap_memory()
+bToB(swapsito, convert_indexes=[0, 1, 2])
+print(swapsito)
+
+#print(memoryGB)
