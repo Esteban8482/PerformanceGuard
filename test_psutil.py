e8b95a9411d690aa78947e72421815bcdad81316
Esteban <esteban.salazar@utp.edu.co>
2023-04-10 13:28:29 -0500

    Avances en la extraccion de data mediante psutil

--- test_psutil.py
+++ test_psutil.py
@@ -19,6 +19,20 @@ def bToB (memory, save = None, convert_indexes = []):
     else:
         print(memoryGB)
 
+# Funcion que estiliza las conexiones de la network (No todos los posibles datos han sido agregados aun)
+def filt_IO_net (connections):
+    aux_net = []
+    for i in connections:
+        if i.status == psutil.CONN_ESTABLISHED:
+            protocol = i.type
+            local_address = f"{i.laddr.ip}{i.laddr.port}"
+            remote_adress = f"{i.raddr.ip}{i.raddr.port}"
+            pid = i.pid
+            aux_net.append(f"{protocol} | {local_address} -> {remote_adress} | PID: {pid}")
+            
+    for connection in aux_net:
+        print(connection)
+
 ## Contador de nucleos e hilos del procesador
 #logical_cpu_count = psutil.cpu_count()
 #fisical_cpu_count = psutil.cpu_count(False)
@@ -32,8 +46,34 @@ def bToB (memory, save = None, convert_indexes = []):
 #print(mem)
 
 ## Memoria SWAP
-swapsito = psutil.swap_memory()
-bToB(swapsito, convert_indexes=[0, 1, 2])
-print(swapsito)
+#swapsito = psutil.swap_memory()
+#bToB(swapsito, convert_indexes=[0, 1, 2])
+#print(swapsito)
+
+## Disco duro datos generales, informacion como el tipo de sistema de archivos
+#disks = psutil.disk_partitions(all = False)
+#print(disks)
+
+## Espacio usado por el disco
+#usageD = psutil.disk_usage('D://')
+#usageC = psutil.disk_usage('/')
+#bToB(usageD, convert_indexes = [0, 1, 2])
+#bToB(usageC, convert_indexes = [0, 1, 2])
+#print(usageD, usageC)
+
+## Performance de IO del disco
+#performance = psutil.disk_io_counters(perdisk = True)
+#performanceC = bToB(performance.get("PhysicalDrive0"), save = True, convert_indexes = [2, 3])
+#performanceD = bToB(performance.get("PhysicalDrive1"), save = True, convert_indexes = [2, 3])
+#print(performanceC, performanceD)
+
+## Entradas y salidas hacia la network desde distintas interfaces
+#netIO = psutil.net_io_counters()
+#bToB(netIO, convert_indexes = [0, 1])
+#print(netIO)
+
+## Detectar conexiones en la network
+conIO = psutil.net_connections()
+filt_IO_net(conIO)
 
 #print(memoryGB)
